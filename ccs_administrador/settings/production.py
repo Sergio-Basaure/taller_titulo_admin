985adabca7331062cc0ed0b2321ce11c5f7b11c1
Sergio Basaure <chechobasaure@gmail.com>
2021-10-24 23:14:04 -0300

    se configura en wsgi y production la ruta de los archivos estaticos

--- ccs_administrador/settings/production.py
+++ ccs_administrador/settings/production.py
@@ -16,3 +16,5 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+STATICFILES_DIRS = (BASE_DIR, 'static')
--- ccs_administrador/wsgi.py
+++ ccs_administrador/wsgi.py
@@ -5,3 +5,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ccs_administrador.settings.production')
 
 application = get_wsgi_application()
+
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
