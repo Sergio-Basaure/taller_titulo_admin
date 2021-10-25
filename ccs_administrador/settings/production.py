c30eb2beaa0cbf96982a9b64e7319c1946ac820e
Sergio Basaure <chechobasaure@gmail.com>
2021-10-24 22:38:25 -0300

    se agrega ALLOWED_HOSTS = ['cssadministrador.herokuapp.com'] para heroku

--- ccs_administrador/settings/production.py
+++ ccs_administrador/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://cssadministrador.herokuapp.com/']
+ALLOWED_HOSTS = ['cssadministrador.herokuapp.com']
 
 
 DATABASES = {
