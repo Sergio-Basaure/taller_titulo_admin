1982f49e700040b42cbebcb3b87b8bf79a00c6d0
Sergio Basaure <chechobasaure@gmail.com>
2021-10-24 22:32:45 -0300

    se modifica la ruta de acceso al admin /

--- ccs_administrador/wsgi.py
+++ ccs_administrador/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ccs_administrador.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ccs_administrador.settings.production')
 
 application = get_wsgi_application()
