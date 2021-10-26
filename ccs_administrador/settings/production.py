bdbbfef9a11a1fc9255a6c0cf51b0497ec631ef3
Sergio Basaure <61486704+Sergio-Basaure@users.noreply.github.com>
2021-10-26 12:59:57 -0300

    Update production.py

--- ccs_administrador/settings/production.py
+++ ccs_administrador/settings/production.py
@@ -6,15 +6,6 @@ DEBUG = True
 ALLOWED_HOSTS = ['cssadministrador.herokuapp.com']
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': ,
-    }
-}
+
 
 STATICFILES_DIRS = (BASE_DIR, 'static')
