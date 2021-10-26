6f4e9ecef4730e0397a91e2f53c99fbea708ec8e
Sergio Basaure <chechobasaure@gmail.com>
2021-10-26 18:02:22 -0300

    se adtualiza proyecto para carga en repositorio

--- ccs_administrador/settings/base.py
+++ ccs_administrador/settings/base.py
@@ -8,9 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
+'django-insecure-w7t&*k-l6(@0cq&)74$7n33k%^p0^)y!2c%(jku3%#dqd#%t!8'
 
 
 # Application definition
