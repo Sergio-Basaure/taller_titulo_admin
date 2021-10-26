3196893a45057cc464fbea6485c5089920a82ac1
Sergio Basaure <chechobasaure@gmail.com>
2021-10-26 18:01:08 -0300

    se adtualiza proyecto para carga en repositorio

--- ccs_administrador/settings/local.py
+++ ccs_administrador/settings/local.py
@@ -6,4 +6,13 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'da0ijl1evhpak6',
+        'USER': 'lnamsplssaupkx',
+        'PASSWORD': 'cfa7ee88a800551c052991d2bf381679abfdc0c92fdc1ae92a0484d783afb6b4',
+        'HOST': 'ec2-52-0-234-93.compute-1.amazonaws.com',
+        'PORT': 5432,
+    }
 }
